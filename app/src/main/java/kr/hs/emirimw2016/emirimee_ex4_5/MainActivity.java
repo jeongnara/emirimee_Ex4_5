a5403e0b2a06ae3984d24c7bc9a3da1a0cca8825
user <w2016@e-mirim.hs.kr>
2021-07-26 13:34:52 +0900

    Edit MainActivity.java(Add Item) Commit

--- app/src/main/java/kr/hs/emirimw2016/emirimee_ex4_5/MainActivity.java
+++ app/src/main/java/kr/hs/emirimw2016/emirimee_ex4_5/MainActivity.java
@@ -3,12 +3,35 @@ package kr.hs.emirimw2016.emirimee_ex4_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> arrayList;
+    ArrayAdapter<String> adapter;
+    EditText editItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editItem = findViewById(R.id.edit_item);
+        arrayList = new ArrayList<String>();
+        ListView list1 = findViewById(R.id.list1);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        list1.setAdapter(adapter);
+        Button btnAdd = findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                arrayList.add(editItem.getText().toString());
+                adapter.notifyDataSetChanged();
+                editItem.setText("");
+            }
+        });
     }
 }
